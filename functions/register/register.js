a70960c16b973e4ae032b1bbc10f21c31b4f05cc
requin883 <requin883@gmail.com>
2022-09-07 08:57:37 -0700

    feat: create jwt token add to user and send email with confirmation link. Add environmental variables

--- functions/register/register.js
+++ functions/register/register.js
@@ -1,51 +1,61 @@
-let connectDB = require ('../connectDB/connectDB')
-let { output } = require('../../utils')
-let bcrypt = require('bcrypt')
-
-
+const connectDB = require('../connectDB/connectDB');
+const { output } = require('../../utils');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { transporter, accountVerOpt } = require('../../Utils')
 exports.handler = async (event) => {
 
     let {
         httpMethod: method,
         queryStringParameters: p
-     } = event;
+    } = event;
 
-
+    
     let client = await connectDB()
     const colUsers = client.db().collection('users');
 
-    if(method == "POST" || method=="OPTIONS"){
-    
-            try {
-             
-             let salt = await bcrypt.genSalt(10);
-             let hash = await bcrypt.hash(p.password, salt)
-                
-             await colUsers.insertOne({ 
-                    
-                    email:p.email,
-                    names:p.names,
-                    lastnames: p.lastnames,
-                    address: p.address,
-                    password:hash,
-                    verify:false
-                    
-            })
+    if (method == "POST" || method == "OPTIONS") {
+
+        try {
+
+            let salt = await bcrypt.genSalt(10);
+            let hash = await bcrypt.hash(p.password, salt);
+
+            const userToken = jwt.sign({
+                email: p.email,
+                password: p.password
+            }, process.env.JWT_SECRET,
+                { expiresIn: "1h" });
+
+            const user = await colUsers.insertOne({
+
+                email: p.email,
+                names: p.names,
+                lastnames: p.lastnames,
+                address: p.address,
+                password: hash,
+                verify: false,
+                verToken: userToken
+
+            });
+
+            const verLink = `${process.env.FRONT_URI}/activate-account/${userToken}`;
+
+            transporter.sendMail(accountVerOpt(p, verLink));
 
             return output(1)
-                
-                
-            } catch (error) {
-                
-                console.log(error)
-            }
-            
-        
 
-        
+        } catch (error) {
+
+            console.log(error)
+        }
 
 
-            
+
+
+
+
+
     }
 
 }
