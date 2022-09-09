070e6c449c7bd981293689089b4fbedc671743fe
requin883 <requin883@gmail.com>
2022-09-09 07:58:16 -0700

    Use a function to send email and process email opt formatted add function to connect to collection

--- functions/register/register.js
+++ functions/register/register.js
@@ -2,13 +2,13 @@ const connectDB = require("../connectDB/connectDB");
 const { output } = require("../../utils");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const nodeMailer = require("nodemailer");
-//const { transporter, accountVerOpt } = require('../../Utils')
+const { transporter, accountVerOpt } = require('../../Utils/nodemailer');
+const connectCollection = require('../../Utils/connectCollection');
+
 exports.handler = async (event) => {
   let { httpMethod: method, queryStringParameters: p } = event;
 
-  let client = await connectDB();
-  const colUsers = client.db().collection("users");
+  const colUsers = connectCollection('users');
 
   if (method == "OPTIONS") {
     return output("hola");
@@ -48,28 +48,6 @@ exports.handler = async (event) => {
             saldo: 0,
             });
 
-            
-
-            const transporter = nodeMailer.createTransport({
-            service: "gmail",
-            port: 587,
-            auth: {
-                user: process.env.NODEMAILER_USER,
-                pass: process.env.NODEMAILER_PW,
-            },
-            });
-
-            const accountVerOpt = (user, verLink) => {
-            let { email, names } = user;
-            return {
-                from: "CryptoCoders",
-                to: email,
-                bbc: process.env.NODEMAILER_USER,
-                subject: `Confirmacion de tu cuenta de CrytoCoders`,
-                html: `<h2>${names}! Para poder activar la cuenta por favor ingresa en el siguiente link <a href=${verLink}>Activar cuenta</a></h2>`,
-            };
-            };
-
             const verLink = `${process.env.FRONT_URI}/activate-account/${userToken}`;
 
             transporter.sendMail(accountVerOpt(p, verLink));
@@ -90,9 +68,9 @@ exports.handler = async (event) => {
         //if (Date.now() >= decoded.exp * 1000) {expiredFlag=true}
         console.log(expiredFlag)
         
-        if (userData[0].verify == false) {// registrado, pero no verificó el correo
+        if (!userData[0].verify) {// registrado, pero no verificó el correo
 
-                if(expiredFlag==true){ // registrado, pero no verificó el correo y el token se venció (ya pasó 1h)
+                if(expiredFlag){ // registrado, pero no verificó el correo y el token se venció (ya pasó 1h)
 
                     let salt = await bcrypt.genSalt(10);
                     let hash = await bcrypt.hash(p.password, salt);
