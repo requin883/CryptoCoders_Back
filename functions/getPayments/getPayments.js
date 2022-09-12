b681b3dbe4555027453180eb968a27910661aaa0
Victor Kneider <victorkneider@gmail.com>
2022-09-12 01:25:24 -0400

    feat: add get user info functions

---
+++ functions/getPayments/getPayments.js
@@ -0,0 +1,30 @@
+let { output } = require('../../utils');
+let connectDB = require('../connectDB/connectDB');
+
+
+exports.handler = async (event) => {
+
+   let {
+      httpMethod: method,
+      queryStringParameters: p
+   } = event;
+
+   
+   let client = await connectDB()
+   const colUsers = client.db().collection('users');
+  
+   if (method == "OPTIONS"){
+      return output('hola')
+   }
+
+   if (method == "GET"  ) {
+
+      let { email } = p;
+      try {
+
+         let user = await colUsers.find({ email }).toArray();
+         return output(user.payments);
+
+      } catch (error) {console.log(error);}
+   }
+}
